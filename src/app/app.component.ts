4f148d99a652c6b04ece5cc5708faafd77f657ae
igor2000xp <igorring@gmail.com>
2023-03-18 00:54:29 +0100

    feat: create template dorm, input, and time 34:00

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-workshop-first';
+  enterName = '';
+
+  userNameSubmitted = false;
+
+  currentDate = new Date();
+
+  submitUserName(): void {
+    this.userNameSubmitted = true;
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000)
+  }
 }
