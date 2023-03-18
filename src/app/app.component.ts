78175ed332469973d240a20f7c59f8e1e40671cd
igor2000xp <igorring@gmail.com>
2023-03-18 01:42:05 +0100

    feat: save User to the localStorage, final 42:15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'angular-workshop-first';
-  enterName = '';
+  currentName = '';
 
   userNameSubmitted = false;
 
   currentDate = new Date();
 
-  submitUserName(): void {
+  private readonly localStorageKey = 'momentumUserName';
+
+  submitUserName(enteredName: string): void {
     this.userNameSubmitted = true;
+    this.currentName = enteredName;
+    localStorage.setItem(this.localStorageKey, this.currentName);
   }
 
   ngOnInit() {
+    // TODO moved to the service
+    const savedName = localStorage.getItem(this.localStorageKey);
+
+    if (savedName) {
+      this.currentName = savedName;
+      this.userNameSubmitted = true;
+
+    }
+
     setInterval(() => {
       this.currentDate = new Date();
     }, 1000)
